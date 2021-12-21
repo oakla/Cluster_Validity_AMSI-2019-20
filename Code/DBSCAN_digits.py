529161e61aefcfd228a5c2e0568588139846942e
Alexander Oakley <a@oakl.org>
2021-12-21 12:13:35 +1100

    improve readability of DBSCAN_digits.py

--- Code/DBSCAN_digits.py
+++ Code/DBSCAN_digits.py
@@ -5,48 +5,77 @@ from sklearn.datasets import load_digits
 
 import NearestNeighbourDistances
 
-# Macro Values
-print_ = True
+# Meanings
+NOISE_CLUSTER_LABEL = -1
+SEED_VALUE = 42
 
-np.random.seed(42)
+# Options
+PRINT = True
 
-#load data
+# init
+np.random.seed(SEED_VALUE)
+
+# load data
 X_digits, y_digits = load_digits(return_X_y=True)
-#
-inputs = scale(X_digits)
-#inputs = X_digits
 
+# Data Transform
+inputs = scale(X_digits)
+
+# get metadata
 n_samples, n_features = inputs.shape
 n_digits = len(np.unique(y_digits))
-true_labels = y_digits
 
+# Fit using default parameters of DBCSCAN
+# fit model
 db = DBSCAN().fit(inputs)
 
+# create a 1D boolean array representing which samples are 'core' samples
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
+
+# creating a new name as a reminder of what db.labels is
 estimated_labels = db.labels_
 
 # Number of clusters in labels, ignoring noise if present.
-n_clusters_ = len(set(estimated_labels)) - (1 if -1 in estimated_labels else 0)
+#   if there is a 'noise' cluster, do not count it in the number of clusters
+n_clusters_ = (len(set(estimated_labels)) 
+                - (1 if NOISE_CLUSTER_LABEL in estimated_labels else 0))
+
+# how many datapoints were counted as noise
 n_noise_ = list(estimated_labels).count(-1)
 
+# output
 print('Estimated number of clusters: %d' % n_clusters_)
 print('Estimated number of noise points: %d' % n_noise_)
 
-print('plotting...')
+
+# Run DBSCAN with custom eps value
+
+# Plot k-distance
+print('plotting k-distance...')
 eps_est = NearestNeighbourDistances.get_k_distance_plot(inputs, 20)
+
 
 eps = eps_est[1][np.argmax(eps_est[0])]
 
-np.random.seed(42)
+# TODO: rewrite to automate use of k_distance for eps
+#   eps (short for epsilon) defines maximum distance between points in a cluster
+#   one recommended way to determine a 'good' value for epsilon is to use the elbow
+#   of a k-distance graph
+np.random.seed(SEED_VALUE)
 db = DBSCAN(eps=4.25).fit(inputs)
 
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
+
+
 estimated_labels = db.labels_
 
 # Number of clusters in labels, ignoring noise if present.
+#   if there is a 'noise' cluster, do not count it in the number of clusters
 n_clusters_ = len(set(estimated_labels)) - (1 if -1 in estimated_labels else 0)
+
+
 n_noise_ = list(estimated_labels).count(-1)
 print("New cluster estimate after using kdist for eps")
 print('Estimated number of clusters: %d' % n_clusters_)
@@ -56,7 +85,7 @@ print('Estimated number of noise points: %d' % n_noise_)
 # What if we did feature extraction instead of just flattening the pixel array?
 
 
-# if print_:
+# if PRINT:
 #     print("n_digits: %d, \t n_samples %d, \t n_features %d"
 #           % (n_digits, n_samples, n_features))
 #     print(78 * '_')
--- Code/NearestNeighbourDistances.py
+++ Code/NearestNeighbourDistances.py
@@ -8,11 +8,15 @@ from matplotlib import pyplot as plt
 
 def get_k_distance_plot(X: np.ndarray, k: int) -> (int, np.ndarray):
 
+    # X (data), k (the number of nearest neighbours that define a datapoints label-identity)
     dist_matrix = kneighbors_graph(X, k, mode='distance').toarray()
     max_dist_to_kth_neighbor = dist_matrix.max(axis=1)
+
+    # plot a histogram of distance to furtherest neighbor of each data point
     n, bins, patches = plt.hist(max_dist_to_kth_neighbor, bins=np.linspace(0,10,20))
     plt.show()
 
+    
     plt.plot(np.sort(max_dist_to_kth_neighbor))
     plt.show()
 
